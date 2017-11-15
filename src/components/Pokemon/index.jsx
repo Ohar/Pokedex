1fca021aa112054cc3bf0da4b6683f473734b09d
Pavel Lysenko <code@ohar.name>
2017-11-15 16:39:45 +0100

    Pokemon: show params

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -6,6 +6,23 @@ function Pokemon ({pokemon}) {
   return (
     <section className='Pokemon'>
       <header className='Pokemon_name'>{pokemon.name}</header>
+      <img
+        className='Pokemon_avatar'
+        width='96'
+        height='96'
+        src={pokemon.sprites.front_default}
+        alt={pokemon.name}
+      />
+      <table className='Pokemon_params'>
+        <tr>
+          <th>Weight</th>
+          <td>{pokemon.weight}</td>
+        </tr>
+        <tr>
+          <th>Type</th>
+          <td>{pokemon.types.map(type => type.type.name).join(', ')}</td>
+        </tr>
+      </table>
     </section>
   )
 }
