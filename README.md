9d92463434322abf46c3ff2182215d2ce0fdd40f
Pavel Lysenko <code@ohar.name>
2017-11-16 20:03:12 +0100

    Add README; remove “config.json” from project

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pokedex
+
+## Install
+
+1. Install Node.js 8.6.0+
+3. `yarn install`
+4. create `config.json` [as described](#configjson-example)
+
+```bash
+node index.js
+```
+
+## Usage
+
+### Development
+
+```bash
+npm run dev
+```
+
+### Production build
+
+```bash
+npm run build
+```
+
+## `config.json` example
+
+```json
+{
+  "API_URL": "http://172.104.147.64/pokedex/api"
+}
+```
--- src/service/api.js
+++ src/service/api.js
@@ -1,18 +1,14 @@
 import { API_URL } from './../../config.json'
 import 'whatwg-fetch'
 
-const URL = __DEV__
-  ? API_URL.dev
-  : API_URL.prod
-
 const API = {
   getPokedex () {
-    return fetch(`${URL}/pokedex`)
+    return fetch(`${API_URL}/pokedex`)
       .then(response => response.json())
       .then(pokedex => pokedex.filter(e => e.id)) // Exclude broken pokemondata
   },
   getTypes () {
-    return fetch(`${URL}/types`)
+    return fetch(`${API_URL}/types`)
       .then(response => response.json())
   },
 }
