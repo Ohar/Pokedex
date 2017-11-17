2594b25d3f2095a287fac09a39542e7d710ec078
Pavel Lysenko <code@ohar.name>
2017-11-17 19:07:17 +0100

    Add “Pokemon” font

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './style.less'
 import PropTypes from 'prop-types'
+import capitalizeFirstLetter from 'capitalize-first-letter'
+import classNames from 'classnames'
+
+const MAX_POKEMON_HEADER_SIZE = 15
 
 function Pokemon ({pokemon}) {
+  const name = capitalizeFirstLetter(pokemon.name)
+
   return (
     <section className='Pokemon'>
-      <header className='Pokemon_name'>{pokemon.name}</header>
+      <header
+        className='Pokemon_header'>
+        <div
+          data-name={name}
+          className={
+            classNames(
+              'Pokemon_name',
+              name.length > MAX_POKEMON_HEADER_SIZE
+                ? 'Pokemon_name-long'
+                : '',
+            )
+          }>
+          {
+            name
+              .split('')
+              .map(
+                (letter, i) => (
+                  <span className='Pokemon_letter' data-letter={letter} key={i}>{letter}</span>
+                ),
+              )
+          }
+        </div>
+      </header>
+
       <img
         className='Pokemon_avatar'
         width='96'
@@ -13,16 +42,17 @@ function Pokemon ({pokemon}) {
         src={pokemon.sprites.front_default}
         alt={pokemon.name}
       />
+
       <table className='Pokemon_params'>
         <tbody>
-          <tr>
-            <th>Weight</th>
-            <td>{pokemon.weight}</td>
-          </tr>
-          <tr>
-            <th>Type</th>
-            <td>{pokemon.types.join(', ')}</td>
-          </tr>
+        <tr>
+          <th className='Pokemon_cell Pokemon_cell-head'>Weight</th>
+          <td className='Pokemon_cell'>{pokemon.weight}</td>
+        </tr>
+        <tr>
+          <th className='Pokemon_cell Pokemon_cell-head'>Type</th>
+          <td className='Pokemon_cell'>{pokemon.types.join(', ')}</td>
+        </tr>
         </tbody>
       </table>
     </section>
